dde482c77c0466f3a3a45539e0f96060747fad0c
github4swapnil <65334233+github4swapnil@users.noreply.github.com>
2020-06-08 17:28:45 +0530

    Update EXEC_DQSP.sql

--- EXEC_DQSP.sql
+++ EXEC_DQSP.sql
@@ -29,3 +29,8 @@ SET ECHO OFF
                 
                 SET TERMOUT OFF
 
+SPOOL report.html
+
+select cast(HEALTHPLAN as char(10)) as HEALTHPLAN, cast(TABLENAME as char(10)) as TABLENAME, cast(COLUMNNAME as char(20)) as COLUMNNAME, cast(TEST_SCENARIO as char(30)) as TEST_SCENARIO, cast(SOURCE_COUNT as char(10)) as SOURCE_COUNT,cast(TARGET_COUNT as char(10)) as TARGET_COUNT, cast(STATUS as char(10)) as STATUS, cast(STATUS_DESC as char(160)) as STATUS_DESC, cast(DQ_REMARKS as char(100)) as DQ_REMARKS from SP_UTILITY_DQ_OUTPUT
+
+SPOOL OFF
