b570d907b14e95d12a87cf4a434be8db25c1c175
github4swapnil <65334233+github4swapnil@users.noreply.github.com>
2020-06-08 16:46:19 +0530

    Update EXEC_DQSP.sql

--- EXEC_DQSP.sql
+++ EXEC_DQSP.sql
@@ -7,3 +7,8 @@ SELECT DISTINCT
 (SELECT COUNT(STATUS)  from SP_Utility_DQ_OUTPUT) AS TOTAL FROM SP_Utility_DQ_OUTPUT
 
 GO
+
+select cast(HEALTHPLAN as char(10)) as HEALTHPLAN, cast(TABLENAME as char(10)) as TABLENAME, cast(COLUMNNAME as char(20)) as COLUMNNAME, cast(TEST_SCENARIO as char(30)) as TEST_SCENARIO, cast(SOURCE_COUNT as char(10)) as SOURCE_COUNT,cast(TARGET_COUNT as char(10)) as TARGET_COUNT, cast(STATUS as char(10)) as STATUS, cast(STATUS_DESC as char(160)) as STATUS_DESC, cast(DQ_REMARKS as char(100)) as DQ_REMARKS from SP_UTILITY_DQ_OUTPUT
+
+GO
+
