2d2984d6eef7c18b28609160f66c5dddec6536dc
github4swapnil <65334233+github4swapnil@users.noreply.github.com>
2020-06-12 18:18:04 +0530

    Update EXEC_DQSP.sql

--- EXEC_DQSP.sql
+++ EXEC_DQSP.sql
@@ -1,6 +1,10 @@
 USE [QACOP1]
 GO
 
+SELECT '------------------OUTPUT OF SQL DQ UTILITY---------------------'
+
+GO
+
 SELECT DISTINCT
 (SELECT COUNT (STATUS) from SP_Utility_DQ_OUTPUT WHERE SP_Utility_DQ_OUTPUT.STATUS = 'FAILED') AS FAILED,
 (SELECT COUNT(STATUS)  from SP_Utility_DQ_OUTPUT WHERE SP_Utility_DQ_OUTPUT.STATUS = 'PASSED') AS PASSED,
@@ -9,6 +13,10 @@ SELECT DISTINCT
 GO
 
 select cast(HEALTHPLAN as char(10)) as HEALTHPLAN, cast(TABLENAME as char(10)) as TABLENAME, cast(COLUMNNAME as char(20)) as COLUMNNAME, cast(TEST_SCENARIO as char(30)) as TEST_SCENARIO, cast(SOURCE_COUNT as char(10)) as SOURCE_COUNT,cast(TARGET_COUNT as char(10)) as TARGET_COUNT, cast(STATUS as char(10)) as STATUS, cast(STATUS_DESC as char(160)) as STATUS_DESC, cast(DQ_REMARKS as char(100)) as DQ_REMARKS from SP_UTILITY_DQ_OUTPUT
+
+GO
+
+SELECT '-------------------OUTPUT OF DT_DR UTILITY-------------------------'
 
 GO
 
